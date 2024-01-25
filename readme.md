c1290c0954e150b293c3a67b56cb35ae78385264
fisnik nez <fisni@MacBook-Air.local>
2024-01-25 11:30:23 +0200

    ensimmäinen kommit

---
+++ readme.md
@@ -0,0 +1,2 @@
+node.js + express
+start dev server: "npm run dev"/"npm start"
---
+++ src/example.mjs
@@ -0,0 +1,6 @@
+const dosomthing =(input) =>{ return "i did somthing" + input};
+
+//console.log(dosomthing("eat"));
+dosomthing = ("drink")
+console,log("doubles",doubles)
+export{dosomthing};
